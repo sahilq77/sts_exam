572dee3a4ce650c36dca2cc37c0ccf9cbaf4b498
sahilq77 <sahil.lekawale@quickensol.in>
2025-07-05 17:33:09 +0530

    unused code removed

--- lib/controller/login/login_controller.dart
+++ lib/controller/login/login_controller.dart
@@ -4,7 +4,6 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 import '../../app_colors.dart';
 import '../../core/network/exceptions.dart';
 import '../../core/network/networkcall.dart';
@@ -19,23 +18,14 @@ class LoginController extends GetxController {
     BuildContext? context,
     required String? mobileNumber,
     required String? password,
-        required String? token,
+    required String? token,
   }) async {
     try {
-      // final jsonBody = {
-      //   "user_type": selechtedUserType!.value,
-      //   "full_name": fullNameControler.value.text,
-      //   "email": emailControler.value.text,
-      //   "mobile_number": emailControler.value.text,
-      //   "gender": selechtedGender!.value,
-      //   "profile_image": "",
-      //   "state": selechtedState!.value,
-      //   "city": selechtedCity!.value,
-      //   "udise_number": uidControler.value.text,
-      //   "school_name": schoolNameControler.value.text,
-      //   "password": passwordControler.value.text,
-      // };
-      final jsonBody = {"mobile_number": mobileNumber, "password": password,"device_token":token};
+      final jsonBody = {
+        "mobile_number": mobileNumber,
+        "password": password,
+        "device_token": token,
+      };
 
       isLoading.value = true;
       // ProgressDialog.showProgressDialog(context);
@@ -57,44 +47,73 @@ class LoginController extends GetxController {
         if (response[0].status == "true") {
           final user = response[0].data;
           log("userid${user.userid}");
-          await AppUtility.setUserInfo(
-            user.userType,
-            user.userid,
-          );
+          await AppUtility.setUserInfo(user.userType, user.userid);
 
-          Get.snackbar('Success', 'Login Success',
-              backgroundColor: AppColors.successColor, colorText: Colors.white);
+          Get.snackbar(
+            'Success',
+            'Login Success',
+            backgroundColor: AppColors.successColor,
+            colorText: Colors.white,
+          );
           Get.offNamed(AppRoutes.home);
           // Get.offNamed('/dashboard');
         } else {
-          Get.snackbar('Error', response[0].message,
-              backgroundColor: AppColors.errorColor, colorText: Colors.white);
+          Get.snackbar(
+            'Error',
+            response[0].message,
+            backgroundColor: AppColors.errorColor,
+            colorText: Colors.white,
+          );
         }
       } else {
         Get.back();
-        Get.snackbar('Error', 'No response from server',
-            backgroundColor: AppColors.errorColor, colorText: Colors.white);
+        Get.snackbar(
+          'Error',
+          'No response from server',
+          backgroundColor: AppColors.errorColor,
+          colorText: Colors.white,
+        );
       }
     } on NoInternetException catch (e) {
       Get.back();
-      Get.snackbar('Error', e.message,
-          backgroundColor: AppColors.errorColor, colorText: Colors.white);
+      Get.snackbar(
+        'Error',
+        e.message,
+        backgroundColor: AppColors.errorColor,
+        colorText: Colors.white,
+      );
     } on TimeoutException catch (e) {
       Get.back();
-      Get.snackbar('Error', e.message,
-          backgroundColor: AppColors.errorColor, colorText: Colors.white);
+      Get.snackbar(
+        'Error',
+        e.message,
+        backgroundColor: AppColors.errorColor,
+        colorText: Colors.white,
+      );
     } on HttpException catch (e) {
       Get.back();
-      Get.snackbar('Error', '${e.message} (Code: ${e.statusCode})',
-          backgroundColor: AppColors.errorColor, colorText: Colors.white);
+      Get.snackbar(
+        'Error',
+        '${e.message} (Code: ${e.statusCode})',
+        backgroundColor: AppColors.errorColor,
+        colorText: Colors.white,
+      );
     } on ParseException catch (e) {
       Get.back();
-      Get.snackbar('Error', e.message,
-          backgroundColor: AppColors.errorColor, colorText: Colors.white);
+      Get.snackbar(
+        'Error',
+        e.message,
+        backgroundColor: AppColors.errorColor,
+        colorText: Colors.white,
+      );
     } catch (e) {
       Get.back();
-      Get.snackbar('Error', 'Unexpected errorColor: $e',
-          backgroundColor: AppColors.errorColor, colorText: Colors.white);
+      Get.snackbar(
+        'Error',
+        'Unexpected errorColor: $e',
+        backgroundColor: AppColors.errorColor,
+        colorText: Colors.white,
+      );
     } finally {
       isLoading.value = false;
     }
