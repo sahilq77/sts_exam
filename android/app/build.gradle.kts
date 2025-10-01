9b8a91a1e358bfceac25a049576bec374158a7e7
sahilq77 <sahil.lekawale@quickensol.in>
2025-10-01 12:12:22 +0530

    sdk version and app version change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     defaultConfig {
         applicationId = "com.quick.stsexam"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
